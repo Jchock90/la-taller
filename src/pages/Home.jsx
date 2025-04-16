b008a2983d56248d1ef1d7e03e39625a03e49b19
Jchock90 <jchockmansilla@gmail.com>
2025-04-16 14:34:29 -0300

    ver.1.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,10 @@ import WhatsAppButton from '../components/WhatsAppButton'
 
 const Home = () => {
   const carouselImages = [
-    'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-    'https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-    'https://images.unsplash.com/photo-1490114538077-0a7f8cb49891?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1744216296/taller2_myzau9.jpg',
+    'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1744216296/taller4_bbctcv.jpg',
+    'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1744216293/taller3_m65xxt.jpg',
+    'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1744216292/taller1_lcxjxg.jpg',
   ]
 
   return (
