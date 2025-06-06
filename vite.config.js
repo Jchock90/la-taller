21cd54eedcf3b03a9c1f92d9749a8b2759dcbf30
Jchock90 <jchockmansilla@gmail.com>
2025-06-06 04:37:54 -0300

    ver.2.4

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'path' // Importación necesaria para usar path
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
     alias: {
