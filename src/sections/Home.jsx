0c50eee858a37330538d67c69cdb09d598977e00
Jesús <jchockmansilla@gmail.com>
2026-03-04 20:46:52 -0300

    feat: add contact section with WhatsApp integration

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -8,7 +8,9 @@ const Home = ({ setCurrentSection }) => {
     'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1744216296/taller2_myzau9.jpg',
     'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1744216296/taller4_bbctcv.jpg',
     'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1744216293/taller3_m65xxt.jpg',
-    'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1744216292/taller1_lcxjxg.jpg'
+    'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1744216292/taller1_lcxjxg.jpg',
+    'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1770750350/IMG_20250401_125636_065_fnnsll.jpg',
+    'https://res.cloudinary.com/dtnkj0wdx/image/upload/t_resize/v1770751215/DSC_4000_kbul1k.jpg'
   ];
 
   return (
